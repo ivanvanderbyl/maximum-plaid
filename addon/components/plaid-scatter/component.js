9729292f58301c1da07620a85c8bb3f75a2f14cd
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-03-27 16:51:55 -0700

    Added plaid-scatter

---
+++ addon/components/plaid-scatter/component.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+import layout from './template';
+import GroupElement from '../../mixins/group-element';
+const { computed } = Ember;
+
+const ScatterComponent = Ember.Component.extend(GroupElement, {
+  layout,
+
+  xScale: null,
+
+  yScale: null,
+
+  values: [],
+
+  positions: computed('values.[]', 'xScale', 'yScale', {
+    get() {
+      const { values, xScale, yScale }
+        = this.getProperties('xScale', 'yScale', 'values');
+
+      return values.map(([timestamp, value]) => {
+        return {
+          x: xScale(timestamp),
+          y: yScale(value),
+        };
+      });
+    }
+  })
+});
+
+ScatterComponent.reopenClass({
+  positionalParams: ['values']
+});
+
+export default ScatterComponent;
