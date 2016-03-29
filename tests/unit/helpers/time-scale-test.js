ff921ae015891882e2e3642606a2f671dc3cd6a9
Spencer P <spencer516@gmail.com>
2016-03-29 14:18:45 -0700

    phantom 1.9 compliance

--- tests/unit/helpers/time-scale-test.js
+++ tests/unit/helpers/time-scale-test.js
@@ -8,43 +8,39 @@ test('it works', function(assert) {
   let result;
 
   result = timeScale([
-    [d(2015, 5, 1), d(2015, 5, 5)],
+    [new Date(2015, 5, 1), new Date(2015, 5, 5)],
     [1, 5]
   ], {});
   assert.ok(result);
-  assert.equal(result(d(2015, 5, 2)), 2, 'date scales');
+  assert.equal(result(new Date(2015, 5, 2)), 2, 'date scales');
 
   result = timeScale([
-    [d(2009, 0, 1, 0, 17), d(2009, 0, 1, 23, 42)]
+    [new Date(2009, 0, 1, 0, 17), new Date(2009, 0, 1, 23, 42)]
   ], {
     nice: 10
   });
   assert.deepEqual(
     result.domain(),
-    [d(2009, 0, 1), d(2009, 0, 2)],
+    [new Date(2009, 0, 1), new Date(2009, 0, 2)],
     'nice works on date scales'
   );
 
   result = timeScale([
-    [d(2009, 0, 1), d(2009, 0, 4)],
+    [new Date(2009, 0, 1), new Date(2009, 0, 4)],
     [0, 10]
   ], {
     round: true
   });
-  assert.equal(result(d(2009, 0, 2)), 3, 'rounding occurs');
-  assert.equal(result(d(2009, 0, 3)), 7, 'rounding occurs');
-  assert.equal(result(d(2009, 0, 4)), 10, 'rounding occurs');
+  assert.equal(result(new Date(2009, 0, 2)), 3, 'rounding occurs');
+  assert.equal(result(new Date(2009, 0, 3)), 7, 'rounding occurs');
+  assert.equal(result(new Date(2009, 0, 4)), 10, 'rounding occurs');
 
   result = timeScale([
-    [d(2009, 0, 1), d(2009, 0, 4)],
+    [new Date(2009, 0, 1), new Date(2009, 0, 4)],
     [0, 10]
   ], {
     clamp: true
   });
-  assert.equal(result(d(2008, 11, 15)), 0, 'min range is 0');
-  assert.equal(result(d(2009, 2, 15)), 10, 'max range is 0');
+  assert.equal(result(new Date(2008, 11, 15)), 0, 'min range is 0');
+  assert.equal(result(new Date(2009, 2, 15)), 10, 'max range is 0');
 });
-
-function d() {
-  return new Date(...arguments);
-}
