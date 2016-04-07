65366e7c94c136996e4a380eeb3e8b5ef5fa83da
Spencer P <spencer516@gmail.com>
2016-04-07 15:05:42 -0700

    don't import broccoli stuff anymore

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 /* jshint node: true */
 'use strict';
-var path = require('path');
-var Funnel = require('broccoli-funnel');
-var mergeTrees = require('broccoli-merge-trees');
 
 module.exports = {
   name: 'ember-d3-scale'
