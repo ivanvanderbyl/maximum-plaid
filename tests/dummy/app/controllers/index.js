522c5b5e280645a2d05cff073e78c8edc51260a5
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-06-03 14:02:01 +0200

    Configure some dummy data from firebase

---
+++ tests/dummy/app/controllers/index.js
@@ -0,0 +1,10 @@
+import Ember from 'ember';
+import computed from 'ember-computed';
+
+export default Ember.Controller.extend({
+
+  responseTimeMean: [],
+
+  curveTension: 1,
+
+});
--- tests/dummy/app/routes/index.js
+++ tests/dummy/app/routes/index.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
+const {
+  inject,
+  RSVP: { Promise },
+  String: { camelize }
+} = Ember;
+
 export default Ember.Route.extend({
+  firebase: inject.service(),
+
+  model() {
+    let ref = this.get('firebase');
+
+    return new Promise((resolve, reject) => {
+      ref.once('value', function(snapshot) {
+        let value = snapshot.val();
+        let data = {};
+        Object.keys(value).map((key) => {
+          data[camelize(key)] = value[key];
+        });
+
+        resolve(data);
+      }, reject);
+    });
+
+  },
+
+  setupController(controller, { responseTimeMean }) {
+    controller.setProperties({ responseTimeMean });
+
+    window.setInterval(() => {
+      controller.set('curveTension', Math.random());
+      console.log(controller.get('curveTension'));
+    }, 2000);
+  }
+
 });
