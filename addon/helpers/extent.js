a1c8223b638f67d4355c1c89a600c6f6bc846edf
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-06-03 16:46:33 +0200

    Added support to extent to reset to zero

--- addon/helpers/extent.js
+++ addon/helpers/extent.js
@@ -2,10 +2,16 @@ import Ember from 'ember';
 import { extent as arrayExtent } from 'd3-array';
 const { get } = Ember;
 
-export function extent([array, accessor]) {
+export function extent([array, accessor], options) {
+
   if (!!accessor) {
     array = array.map((d) => get(d, accessor));
   }
+
+  if (options && options.toZero === true) {
+    array.push(0);
+  }
+
   return arrayExtent(array);
 }
 
