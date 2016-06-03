88b4bb6f4df1df40d4254b9da07871a6118f64db
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-06-03 14:00:36 +0200

    Added curve helper

---
+++ tests/unit/helpers/curve-test.js
@@ -0,0 +1,10 @@
+import { curve } from 'dummy/helpers/curve';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | curve');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = curve([42]);
+  assert.ok(result);
+});
