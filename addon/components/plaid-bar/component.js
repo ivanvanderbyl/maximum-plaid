75de2a54ab42eca203433352960b9bfa47069bcc
Kyle Turney <kyle@dialexa.com>
2016-06-20 12:01:29 -0500

    add orientation to plaid-bar

--- addon/components/plaid-bar/component.js
+++ addon/components/plaid-bar/component.js
@@ -2,13 +2,23 @@ import Ember from 'ember';
 import GroupElement from '../../mixins/group-element';
 
 const {
+  assert,
   Component,
   getProperties,
-  run: { scheduleOnce }
+  run: { scheduleOnce },
+  typeOf
 } = Ember;
 
 const PlaidBarComponent = Component.extend(GroupElement, {
   layout: null,
+
+  /**
+   * Represents the bar orientation. May be "vertical" or "horizontal"
+   *
+   * @public
+   * @type {String}
+   */
+  orientation: 'vertical',
 
   /**
     xScale function
@@ -41,21 +51,44 @@ const PlaidBarComponent = Component.extend(GroupElement, {
 
   didReceiveAttrs() {
     this._super(...arguments);
+
+    let orientation = this.get('orientation');
+
+    assert(`bar chart orientation must be in {vertical,horizontal}, was "${orientation}"`,
+      orientation === 'vertical' || orientation === 'horizontal');
+
+    let checkScale = orientation === 'vertical' ? 'xScale' : 'yScale';
+
+    assert(`${checkScale} must be a band-scale for ${orientation} bar charts`, typeOf(this.get(checkScale).bandwidth) === 'function');
+
     scheduleOnce('afterRender', this, this.drawBars);
   },
 
   drawBars() {
-    let { values, xScale, yScale, fill, fillOpacity } =
-      getProperties(this, 'values', 'xScale', 'yScale', 'fill', 'fillOpacity');
+    let { values, xScale, yScale, fill, fillOpacity, orientation } =
+      getProperties(this, 'values', 'xScale', 'yScale', 'fill', 'fillOpacity', 'orientation');
 
-    let height = Math.max(...yScale.range());
+    let x, width, y, height;
+
+    if (orientation === 'vertical') {
+      let maxHeight = Math.max(...yScale.range());
+      x = (d) => xScale(d[0]);
+      width = xScale.bandwidth();
+      y = (d) => yScale(d[1]);
+      height = (d) => maxHeight - yScale(d[1]);
+    } else {
+      x = Math.min(...xScale.range());
+      width = (d) => xScale(d[0]);
+      y = (d) => yScale(d[1]);
+      height = yScale.bandwidth();
+    }
 
     this.selection.selectAll('.bar').data(values).enter().append('rect')
       .attr('class', 'bar')
-      .attr('x', (d) => xScale(d[0]))
-      .attr('width', () => xScale.bandwidth())
-      .attr('y', (d) => yScale(d[1]))
-      .attr('height', (d) => height - yScale(d[1]))
+      .attr('x', x)
+      .attr('width', width)
+      .attr('y', y)
+      .attr('height', height)
       .attr('fill', fill)
       .attr('fillOpacity', fillOpacity);
   }
