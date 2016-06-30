d3f5d079742d587578c330f72d042918ba732546
Ivan Vanderbyl <ivanvanderbyl@gmail.com>
2016-06-30 15:39:27 +0100

    Display the donut

--- tests/dummy/app/controllers/index.js
+++ tests/dummy/app/controllers/index.js
@@ -1,6 +1,14 @@
 import Ember from 'ember';
 export default Ember.Controller.extend({
 
-  responseTimeMean: []
+  responseTimeMean: [],
+
+  donutData: [
+    { timestamp: 1450345920000, value: 1, projectId: 200 },
+    { timestamp: 1450345930000, value: 2, projectId: 200 },
+    { timestamp: 1450345940000, value: 3, projectId: 200 },
+    { timestamp: 1450345950000, value: 4, projectId: 200 },
+    { timestamp: 1450345960000, value: 5, projectId: 200 }
+  ]
 
 });
